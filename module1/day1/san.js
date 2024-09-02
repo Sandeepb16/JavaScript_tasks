dfb252f5f578c1e625141e926d8f379cffa1b472
Sandeepb16 <kondasandeepreddy15@gmail.com>
2024-09-02 15:43:03 +0530

    added js tasks files

---
+++ module1/day1/san.js
@@ -0,0 +1,28 @@
+console.log("hi this is my first js program")
+
+3
+
+// var name="sandeep";
+// let name1="vikas";
+// const dateofbirth=2002;
+// console.log(name);
+// console.log(name1);
+// console.log(dateofbirth);
+
+
+// let na="k.sandeep reddy";
+// let age = 22;
+// let male=true;
+// let nothing;
+// let eng= null;
+
+// console.log(typeof(na));
+// console.log(typeof(age));
+// console.log(typeof(male));
+// console.log(typeof(nothing));
+// console.log(typeof(eng));
+
+
+debugger;
+var a="hi ra mama"
+let b=2;
