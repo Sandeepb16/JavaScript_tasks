09009fc222c0992d071bcb8ebb45fcfaf682d396
Sandeepb16 <kondasandeepreddy15@gmail.com>
2024-09-06 15:41:17 +0530

    changed file name

---
+++ module1/day2-scopes/operators.js
@@ -0,0 +1,41 @@
+
+// 1. Perform arithmetic and assignment operations on two variables.
+
+let a=10;
+let b=4;
+
+// arithmetic operators 
+
+console.log(a+b);
+// output -14
+console.log(a-b);
+// output -6
+console.log(a*b);
+// output -40
+console.log(a/b);
+// output -2.5
+console.log(a%b);
+// output -2
+console.log(a**b);
+// output -10000
+
+
+// assignment operators
+a+=20;
+console.log(a)
+// output -- 30
+a-=10;
+console.log(a)
+// output -- 20
+a*=20;
+console.log(a)
+// output -- 400
+a/=20;
+console.log(a)
+// output -- 20
+a%=20;
+console.log(a)
+// output -- 0
+a**=20;
+console.log(a)
+// output -- 0
